a628cdd9f422f29a5a5e20a2d1dc005e2eb1b8c1
manleuc <manlu.chen08@gmail.com>
2024-11-23 23:24:29 -0500

    connection to db, added 3 pages

---
+++ frontend/src/createuser.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const CreateUser = () => {
+    return (
+        <div><form action="/action_page.php">
+            <div class="mb-3 mt-3">
+                <label for="username" class="form-label">Username:</label>
+                <input 
+                type="text" 
+                class="form-control" 
+                id="username" 
+                placeholder="Enter username" 
+                name="username" />
+            </div>
+            <div class="mb-3">
+                <label for="fname" class="form-label">First Name:</label>
+                <input 
+                type="text" 
+                class="form-control" 
+                id="fname"
+                placeholder="Enter first name "
+                name="fname" />
+            </div>
+            <div class="mb-3">
+                <label for="lname" class="form-label">Last Name:</label>
+                <input 
+                type="text" 
+                class="form-control" 
+                id="lname"
+                placeholder="Enter last name"
+                name="lname" />
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address:</label>
+                <input 
+                type="text" 
+                class="form-control" 
+                id="address"
+                placeholder="Enter address"
+                name="address" />
+            </div>
+            <div class="mb-3">
+                <label for="bdate" class="form-label">Birthdate:</label>
+                <input 
+                type="date" 
+                class="form-control" 
+                id="bdate"
+                name="bdate" />
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form></div>
+    )
+}
+
+
+export default CreateUser
---
+++ server/server.js
@@ -0,0 +1,102 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '0668', //change this
+    database: 'business_supply'
+})
+
+
+app.listen(8081, ()=> {
+    console.log("Listening");
+})
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err.message);
+//     } else {
+//         console.log('Connected to the MySQL database!');
+//     }
+// });
+
+db.connect();
+
+db.query('SELECT * from users', (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log(rows);
+})
+
+
+app.get('/', (req, res) => {
+    const users = "SELECT * from users;";
+    db.query(users, (err, data) => {
+        if(err){
+            return res.json({Error: err})
+        }
+        return res.json(data)
+    })
+})
+
+app.post('/create', (req, res) => {
+    const users = "insert into users (username, fname, lname, address, birthdate) values(?); ";
+    const values = [       
+        req.body.username,
+        req.body.fname,
+        req.body.lname,
+        req.body.address,
+        req.body.birthdate]
+
+    db.query(sql, [values], (err, data) => {
+        if(err){
+            return res.json({Error: "Error"})
+        }
+        return res.json(data)
+    })
+})
+
+
+
+app.put('/update/:username', (req, res) => {
+    const users = "update users set fname = ?, lname = ?, address = ?, birthdate = ? where username = ?; ";
+    const values = [       
+        req.body.username,
+        req.body.fname,
+        req.body.lname,
+        req.body.address,
+        req.body.birthdate]
+    const username = req.params.username;
+    db.query(sql, [...values, username], (err, data) => {
+        if(err){
+            return res.json({Error: "Error"})
+        }
+        return res.json(data)
+    })
+})
+
+
+app.delete('/delete/:username', (req, res) => {
+    const users = "delete from users where username = ?; ";
+    const username = req.params.username;
+    db.query(sql, [username], (err, data) => {
+        if(err){
+            return res.json({Error: "Error"})
+        }
+        return res.json(data)
+    })
+})
+
+
+
+
+
+
+
+// db.end();
